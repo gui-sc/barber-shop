a20188329b98ad846105495668db00f751153ca5
guilherme coelho <guilhermeifc7@gmail.com>
2024-01-30 21:42:52 -0300

    feat: add initial booking component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,6 +3,7 @@ import Header from "../_components/header";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import { Search } from "lucide-react";
+import BookingItem from "../_components/booking-item";
 
 export default function Home() {
   return (
@@ -20,6 +21,10 @@ export default function Home() {
       <div className="px-5 mt-6">
         <Search />
 
+      </div>
+      <div className="px-5 mt-6">
+        <h2 className="text-xs uppercase text-gray-400 font-bold mb-3">agendamentos</h2>
+        <BookingItem/>
       </div>
     </div>
   );
