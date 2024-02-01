089f5cce6622f325881448c0f8deca893e1f1fb8
guilherme coelho <guilhermeifc7@gmail.com>
2024-02-01 18:21:53 -0300

    feat: add menu

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -3,15 +3,26 @@ import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
 
 const Header = () => {
     return (
         <Card>
             <CardContent className="p-5 justify-between flex flex-row items-center">
                 <Image src="/vercel.svg" alt="" width={80} height={120} />
-                <Button variant="outline" size="icon">
-                    <MenuIcon size={14} />
-                </Button>
+                <Sheet>
+                    <SheetTrigger asChild>
+                        <Button variant="outline" size="icon">
+                            <MenuIcon size={14} />
+                        </Button>
+                    </SheetTrigger>
+
+                    <SheetContent className="p-0">
+                        <SheetHeader className="text-left border-b border-solid border-secondary p-5">
+                            <SheetTitle>Menu</SheetTitle>
+                        </SheetHeader>
+                    </SheetContent>
+                </Sheet>
 
             </CardContent>
         </Card>
